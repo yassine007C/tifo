9a77078ee0b8c3001ec2da2744f7a09d81a22178
yassine007C <104086968+yassine007C@users.noreply.github.com>
2026-06-20 20:42:06 +0100

    Update main.tsx

--- artifacts/tifo/src/main.tsx
+++ artifacts/tifo/src/main.tsx
@@ -4,5 +4,4 @@ import "./index.css";
 import Login from "./pages/login";
 
 createRoot(document.getElementById("root")!).render(<App />);
-<Route path="/login" component={Login} /> 
-// أو <Route path="/login" element={<Login />} /> حسب مكتبة التوجيه لديك
+
